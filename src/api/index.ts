8597ea9e3974b3f37692af9bc2f01f9034908f13
hyan-kelwin <hyankelwin@hotmail.com>
2022-06-27 21:24:52 -0300

    refactor: delete files

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL:'http://781c-177-11-187-30.ngrok.io'
-})
--- src/pages/Cadastro/cadastro.validator.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const cadastroValidator = yup.object({
-    email: yup.string().email('Informe um email válido').required('Campo obrigatório'),
-    password: yup.string().min(6,'Senha deve ter pelo menos 6 caracteres').required('Campo obrigatório'),
-    passwordConfirm: yup.string()
-        .oneOf([yup.ref('password'), null], 'As senhas devem ser iguais'),
-}).required();
